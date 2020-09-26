b26fe8b7cf6cb837f8a930d39b63cf6383561a8a
snymax <snymax@gmail.com>
2020-09-26 15:28:38 -0400

    laid foundation

--- Sources/MacColorPicker/MacColorPicker.swift
+++ Sources/MacColorPicker/MacColorPicker.swift
@@ -1,3 +1,20 @@
-struct MacColorPicker {
-    var text = "Hello, World!"
+//
+//  File.swift
+//  
+//
+//  Created by James Irwin on 9/26/20.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, macOS 10.0, *)
+public struct MacColorPicker: View {
+    @Binding var color: NSColor
+    
+    var body: some View {
+        Circle()
+            .fill(Color(color))
+            .overlay(Circle()
+                        .strokeBorder(Color.white, lineWidth: 1))
+    }
 }
